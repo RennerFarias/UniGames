6c72dc719f689dc472eaf8254e92c4224582789d
Artur Melo <91603735+Jose-si@users.noreply.github.com>
2026-09-01 12:05:41 -0300

    Push de teste inicial com graphql
    teste inicial

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,23 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8']);
+
 require('dotenv').config();
+
 const express = require('express');
 const conectarBanco = require('./database');
-const jogoRoutes = require('./routes/jogoRoutes');
-const revendaRoutes = require('./routes/revendaRoutes');
+
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
+
 app.use(express.json());
+
 conectarBanco();
-app.use(jogoRoutes);
-app.use(revendaRoutes);
+
+app.use(authRoutes);
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
