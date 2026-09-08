f85da5472b880db8d8c1d7d77e716e37f28f496e
Rafael Barbosa <barbosarafaelmello@gmail.com>
2026-09-08 08:39:08 -0300

    autorizaçao de jogos auterada

--- backend/src/routes/jogoRoutes.js
+++ backend/src/routes/jogoRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { autenticar } = require('../middlewares/authMiddleware');
+const { autenticar, autorizar } = require('../middlewares/authMiddleware');
 const {
     cadastrarJogo,
     listarJogos,
@@ -13,7 +13,7 @@ router.get('/jogos', listarJogos);
 router.get('/jogos/:id', obterJogoPorId);
 
 // Rotas protegidas por JWT
-router.post('/jogos', autenticar, cadastrarJogo);
+router.post('/jogos', autenticar, autorizar('admin'), cadastrarJogo);
 router.put('/jogos/:id', autenticar, atualizarJogo);
 router.delete('/jogos/:id', autenticar, deletarJogo);
 
